f8f967ce68d54c870d81a3682e67c04df085f971
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-06-14 11:45:12 +0530

    View Relatives Modal Added. Page Validation Added.

---
+++ FarmerRegistrationAngularApp/src/app/modalComponents/view-relatives-modal/view-relatives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-relatives',
+  templateUrl: './view-relatives.component.html',
+  styleUrls: ['./view-relatives.component.css']
+})
+export class ViewRelativesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
