abb71da8c2786a0ed64640fdc9c441dc06f5405b
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-07-06 19:45:37 +0530

    View Relatives Modal Added. Page Validation Added. Edit Farmer Function Added. View Relatives Page Added(View Relation Component Added)

---
+++ FarmerRegistrationAngularApp/src/app/components/relative/relative.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relative',
+  templateUrl: './relative.component.html',
+  styleUrls: ['./relative.component.css']
+})
+export class RelativeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
